68f7450afa3bf0f16c85887eb40d5b65104752a1
PramAhmad <pramuditaadit2810@gmail.com>
2025-06-04 20:43:45 +0700

    feat: Update routes in web.php to improve code formatting and readability

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[HomeController::class,'home'])->name('home');
+Route::get('/', [HomeController::class, 'home'])->name('home');
+Route::get('/activities', [HomeController::class, 'activities'])->name('activities');
+Route::get('/menfess', [HomeController::class, 'menfess'])->name('menfess');
+Route::get('/journals', [HomeController::class, 'journals'])->name('journals');
+Route::get('/bank-materi', [HomeController::class, 'bankMateri'])->name('bank-materi');
