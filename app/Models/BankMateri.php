29eb5e3026f4e7021cc680982621db28ad1f861f
PramAhmad <pramuditaadit2810@gmail.com>
2025-06-05 20:36:24 +0700

    feat: add download, view, and like count attributes with corresponding methods in BankMateri model

--- app/Models/BankMateri.php
+++ app/Models/BankMateri.php
@@ -22,11 +22,23 @@ class BankMateri extends Model
         'tingkat_kesulitan',
         'penulis',
         'tanggal_publikasi',
+        'download_count',
+        'view_count',
+        'like_count',
     ];
 
     protected $casts = [
         'is_draft' => 'boolean',
         'tanggal_publikasi' => 'date',
+        'download_count' => 'integer',
+        'view_count' => 'integer',
+        'like_count' => 'integer',
+    ];
+
+    protected $attributes = [
+        'download_count' => 0,
+        'view_count' => 0,
+        'like_count' => 0,
     ];
 
     public function mataKuliah(): BelongsTo
@@ -38,5 +50,43 @@ class BankMateri extends Model
     {
         return $this->hasMany(BankMateriFile::class);
     }
+    
+    // Scope untuk filtering
+    public function scopePublished($query)
+    {
+        return $query->where('is_draft', false);
+    }
+    
+    public function scopeByMataKuliah($query, $mataKuliah)
+    {
+        return $query->whereHas('mataKuliah', function($q) use ($mataKuliah) {
+            $q->where('nama', 'like', "%{$mataKuliah}%");
+        });
+    }
+    
+    public function scopeByKategori($query, $kategori)
+    {
+        return $query->where('kategori', $kategori);
+    }
+    
+    public function scopeByTingkat($query, $tingkat)
+    {
+        return $query->where('tingkat_kesulitan', $tingkat);
+    }
+    
+    // Helper methods
+    public function incrementDownload()
+    {
+        $this->increment('download_count');
+    }
+    
+    public function incrementView()
+    {
+        $this->increment('view_count');
+    }
+    
+    public function incrementLike()
+    {
+        $this->increment('like_count');
+    }
 }
-
