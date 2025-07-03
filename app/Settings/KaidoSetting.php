d5acc081412a276450f4d358e917d9003bf17d69
PramAhmad <pramuditaadit2810@gmail.com>
2025-07-03 21:12:44 +0700

    refactor: add additional properties to KaidoSetting class for enhanced configuration

--- app/Settings/KaidoSetting.php
+++ app/Settings/KaidoSetting.php
@@ -2,21 +2,33 @@
 
 namespace App\Settings;
 
-use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 use Spatie\LaravelSettings\Settings;
 
 class KaidoSetting extends Settings
 {
     public string $site_name;
-
+    public string $site_description;
     public bool $site_active;
     public bool $registration_enabled;
     public bool $login_enabled;
     public bool $password_reset_enabled;
     public bool $sso_enabled;
+    public bool $maintenance_mode;
+    public string $contact_email;
+    public string $instagram_url;
+    public string $facebook_url;
+    public string $twitter_url;
+    public string $linkedin_url;
 
     public static function group(): string
     {
         return 'KaidoSetting';
+    }
+
+    public static function encrypted(): array
+    {
+        return [
+            // Jika ada field yang perlu dienkripsi, tambahkan di sini
+        ];
     }
 }
