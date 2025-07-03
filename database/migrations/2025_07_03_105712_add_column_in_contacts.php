e0701972f521ec3efcf4ca38fa53358c82b8a61a
PramAhmad <pramuditaadit2810@gmail.com>
2025-07-03 18:19:59 +0700

    feat: add email, subject, message, phone, and status columns to contacts table

---
+++ database/migrations/2025_07_03_105712_add_column_in_contacts.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->string('email')->nullable()->after('name');
+            $table->string('subject')->nullable()->after('email');
+            $table->text('message')->nullable()->after('subject');
+            $table->string('phone')->nullable()->after('message');
+            $table->enum('status', ['pending', 'replied', 'resolved'])->default('pending')->after('phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            //
+        });
+    }
+};
